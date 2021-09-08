4a02d58dfc6acb0b6a6d954ad602899d8d764a14
Kweon Choi <kweon.choi@vuno.co>
2021-09-08 10:57:59 +0900

    Created a new repsitory and moved python from the original

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # python
+
+ - Read the book and took a look at some points I did not know
+ - Did not really read every single words from the book
+ - I will need to learn more from coding with python itself
+
+
+ updated Sep 08 2021
---
+++ learning_python.py
@@ -0,0 +1,16 @@
+"""
+I have read the book and learned something I did not know
+The author seems to be a 'true believer' of python
+The book taught me to write code clean and python-like
+The book was not a python tutorial, but more like a guidebook for a junior
+developer with experience with other programming language, or for someone already
+using python but looking for a better way to organize the code
+I will have to read the book whenever I need to look back into the skills I have read
+Meaning that, I did not really went through the book. I actually skipped many parts.
+But I do appriciate this book as a good teacher and I am sure that the book will be
+a good friend as I start working with python.
+But for now, that I have some experience with python,
+"""
+
+print("hello world!")
+
